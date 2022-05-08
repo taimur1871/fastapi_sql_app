f1e47946577b26e3bf6f9fbb47f65e265e03e05e
taimur1871 <taimur1871@gmail.com>
2022-05-07 22:54:19 -0600

    Added alembic migrations

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # Library imports
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime, func
 
 Base = declarative_base()
@@ -18,13 +18,16 @@ class BitData(Base):
     bit_name = Column(String, default=None, nullable=False)
     bit_size = Column(Float, default=0, nullable=False)
     bit_type = Column(String, default="", nullable=False)
-    mfg = Column(String, default="", nullable=False)
-    serial_no = Column(String, default="", nullable=False)
+    serial_num = Column(String, default="", nullable=False)
     depth_in = Column(Float, default=0.0, nullable=True)
     depth_out = Column(Float, default=0.0, nullable=True)
     distance = Column(Float, default=0.0, nullable=True)
     hours = Column(Float, default=0.0, nullable=True)
     rop = Column(Float, default=0.0, nullable=True)
+    mfg = Column(Integer, ForeignKey('manufacturer.id'), nullable=False)
+    mfg_id = relationship('Manufacturer', foreign_keys='bit_data.mnaufacturer.id', post_update=True, lazy='select')
+    well_name_id = Column(Integer, ForeignKey('well_data.id'), nullable=False)
+    well_name = relationship('WellData', foreign_keys='bit_data.well_data.id', post_update=True, lazy='select')
 
 
 class WellData(Base):
@@ -57,6 +60,13 @@ class DullGrade(Base):
     gauge = Column(Float, default=0.0, nullable=True)
     other = Column(String, default="", nullable=True)
     reason_pulled = Column(String, default="", nullable=True)
+    mfg = Column(Integer, ForeignKey('manufacturer.id'), nullable=False)
+    mfg_id = relationship('Manufacturer', foreign_keys='bit_data.mnaufacturer.id', post_update=True, lazy='select')
+    well_name_id = Column(Integer, ForeignKey('well_data.id'), nullable=False)
+    well_name = relationship('WellData', foreign_keys='bit_data.well_data.id', post_update=True, lazy='select')
+    bit_serial_num = Column(String, ForeignKey('bit_data.serial_num'), nullable=False)
+    bit_serial_num_id = relationship('BitData', foreign_keys='bit_data.bit_data.serial_num', post_update=True,
+                                     lazy='select')
 
 
 if __name__ == '__main__':
